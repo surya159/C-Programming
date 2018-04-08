a6a67b00cacef4c93b003bd49bd98a57a61908cf
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-08 23:09:57 +0530

    Create NoBtw1and10.c

---
+++ Beginner/set5/NoBtw1and10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(void) {
+	// your code goes here
+	int i;
+	scanf("%d",&i);
+	if(i>=1 && i<=10) {
+		printf("Yes"); 
+	}
+	else
+		printf("No");
+		
+	return 0;
+}
