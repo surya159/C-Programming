5f6b37d7797e624c05f601d3dd06c41cd95b0866
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-08 23:33:00 +0530

    Create MinMaxEle.c

---
+++ Beginner/set5/MinMaxEle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,a[n],t;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+		if(a[i]>a[j])
+		{
+			t=a[i];
+			a[i]=a[j];
+			a[j]=t;
+		}}
+	}
+	printf("%d %d",a[0],a[n-1]);
+		
+	return 0;
+}
