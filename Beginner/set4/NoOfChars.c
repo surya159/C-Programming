9eddb2baf6058cbb86b29e0712725b4e4942b43b
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-08 17:25:51 +0530

    Create NoOfChars.c

---
+++ Beginner/set4/NoOfChars.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	// your code goes here
+	char str[20];
+	int n,count=0;
+	scanf(" %[^\t\n]s",str);
+	n=strlen(str);
+	for(int i=0;i<n;i++)
+	{
+	 if(str[i]>='a'&& str[i]<='z'|| str[i]>='A'&& str[i]<='Z')
+	 {
+	     count++;
+	 }
+	}
+	printf("%d",count);
+	
+	return 0;
+}
