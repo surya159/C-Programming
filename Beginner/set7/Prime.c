ff210925dfa633aaace25f70ff2a8e869755260b
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-09 21:56:50 +0530

    Create Prime.c

---
+++ Beginner/set7/Prime.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+	int n,count=0;
+	scanf("%d",&n);
+	for(int i=2;i<n/2;i++) {
+		if(n%i == 0) {
+			count++;
+			
+		}
+	}
+	if(count==0) {
+		printf("yes");
+	}
+	else {
+		printf("no");
+	}
+	return 0;
+	
+}
