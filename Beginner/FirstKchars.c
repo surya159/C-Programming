375c767e93573280eb98ef889033b950ef2c92ed
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-09 21:08:09 +0530

    Create FirstKchars.c

---
+++ Beginner/FirstKchars.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   char s[20];
+   scanf("%s",s);
+   int k;
+   scanf("%d",&n);
+   
+   for(int i=0;i<k;i++)
+   {
+   	printf("%c",s[i]);
+   }
+   return 0;
+}
