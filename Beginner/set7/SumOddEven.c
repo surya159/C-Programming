02641151e127a00dfbef71aeab4e126a9533f994
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-09 21:39:07 +0530

    Create SumOddEven.c

---
+++ Beginner/set7/SumOddEven.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,m;
+   scanf("%d%d",&n,&m);
+   m=m+n;
+   if(m%2 == 0){
+   	printf("even");
+   }
+   else
+	  printf("odd");
+ 		
+   
+   return 0;
+}
