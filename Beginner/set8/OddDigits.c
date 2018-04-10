13c0e350ce62ab2e50c32dd0bf117e86b2fa9edb
surya159 <34645682+surya159@users.noreply.github.com>
2018-04-10 21:18:32 +0530

    Update OddDigits.c

--- Beginner/set8/OddDigits.c
+++ Beginner/set8/OddDigits.c
@@ -4,8 +4,10 @@
 int main(void) {
 	char s[20];
 	scanf("%s",s);
-	for(int i=1;i<=strlen(s);i=i+2) {
+	for(int i=0;i<=strlen(s);i++) {
+		if(s[i]%2 != 0) {
 		printf("%c ",s[i]);
+	}
 	}
 
 	return 0;
