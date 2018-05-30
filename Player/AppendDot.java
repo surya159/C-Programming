21dc83c5318fb2c14b631731ce559aedcc5795d5
surya159 <34645682+surya159@users.noreply.github.com>
2018-05-30 22:42:41 +0530

    Create AppendDot.java

---
+++ Player/AppendDot.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AppendDOt {
+    public static void main(String args[]) {
+        Scanner sc= new Scanner(System.in);
+        String s=sc.next();
+        System.out.println(s + ".");
+    }
+}
