d59a1be460afaa40daf265e328a467d4b8b220f9
surya159 <34645682+surya159@users.noreply.github.com>
2018-06-02 18:53:45 +0530

    Create SortStrings.java

---
+++ Player/set3/SortStrings.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class SortStrings {
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        String names[] = new String[n];
+        Scanner s1 = new Scanner(System.in);
+        for(int i = 0; i < n; i++)
+        {
+            names[i] = s1.next();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (names[i].compareTo(names[j])>0) 
+                {
+                    String temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            System.out.print(names[i] + " " );
+        }
+        
+    }
+}
