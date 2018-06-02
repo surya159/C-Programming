63e140f2d0b4f36b09b5b801edbc8d127faaffb0
surya159 <34645682+surya159@users.noreply.github.com>
2018-06-02 13:28:11 +0530

    Update gcd.java

--- Beginner/set9/gcd.java
+++ Beginner/set9/gcd.java
@@ -5,7 +5,7 @@ public class GCD {
         int n1=sc.nextInt();
         int n2=sc.nextInt();
         int gcd=1;
-        for(int i=1; i<n1 && i<n2;++i) {
+        for(int i=1; i<=n1 && i<=n2;++i) {
             if(n1%i==0 && n2%i==0) {
                 gcd=i;
             }
