fd7474c2068763825f033687c5b14962a57031f7
surya159 <34645682+surya159@users.noreply.github.com>
2018-06-03 13:11:18 +0530

    Create BalancingParanthesis.cpp

---
+++ Player/set4/BalancingParanthesis.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+bool areParanthesisBalanced(char expr[])
+{
+    stack<char> s;
+    char a,b,c;
+ 
+    for(int i=0; i<strlen(expr); i++)
+    {
+        if (expr[i]=='('||expr[i]=='['||expr[i]=='{')
+        {
+            s.push(expr[i]);
+        }
+        else
+        {
+            switch (expr[i])
+            {
+            case ')':
+             
+                if(s.empty()) 
+                    return false;
+                a = s.top();
+                s.pop();
+                if (a=='{' || a=='[')
+                    return false;
+                break;
+                
+            case '}':
+                 
+                if(s.empty()) 
+                    return false;
+                 b = s.top();
+                s.pop();
+                if (b=='('||b=='[')
+                    return false;
+                break;
+                
+            case ']':
+ 
+                if(s.empty()) 
+                    return false;
+                 
+                c=s.top();
+                s.pop();
+                if (c=='('||c=='{')
+                    return false;
+                break;
+            }
+        }
+    }
+ 
+    if (s.empty())
+        return true;
+    else
+        return false;
+}
+ 
+int main()
+{
+    char expr[20];
+    cin >> setw(20) >> expr;
+    if(areParanthesisBalanced(expr))
+        cout<<"Balanced";
+    else
+        cout<<"Not Balanced";
+    return 0;
+}
