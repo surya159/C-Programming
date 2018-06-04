c5a6d92fc2ff14d565994af7b6c9b989379ab853
surya159 <34645682+surya159@users.noreply.github.com>
2018-06-05 00:09:28 +0530

    Create MaxEleBitonicArr.java

---
+++ Player/set8/MaxEleBitonicArr.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]= new int[n];
+		
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		int max=a[0];
+		for(int i=1;i<n ;i++) {
+			if(a[i]>max) {
+			max=a[i];
+			}
+			
+		}
+		System.out.print(max);
+		
+	}
+}
