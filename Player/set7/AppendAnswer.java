dffd9fe1c9445ac1fc6be0675d4c9e08d7891cae
surya159 <34645682+surya159@users.noreply.github.com>
2018-06-04 23:43:29 +0530

    Create AppendAnswer.java

---
+++ Player/set7/AppendAnswer.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		String s= sc.nextLine();
+		System.out.print(s + " Answer");
+	}
+}
